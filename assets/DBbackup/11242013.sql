a63d2a965624641d960a1c96d9dee406f06bf3df
Andrew James Ramirez <avramirez@dynamicobjx.com>
2013-11-24 09:37:26 +0800

    added db backup

--- assets/DBbackup/11242013.sql
+++ assets/DBbackup/11242013.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 22, 2013 at 03:31 PM
+-- Generation Time: Nov 24, 2013 at 02:36 AM
 -- Server version: 5.5.22
 -- PHP Version: 5.4.19
 
@@ -21,6 +21,29 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `vet_app` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `vet_app`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE IF NOT EXISTS `products` (
+  `objectId` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(200) NOT NULL,
+  `product_quantity` int(11) NOT NULL,
+  `product_price` int(11) NOT NULL,
+  `product_type` varchar(200) NOT NULL,
+  PRIMARY KEY (`objectId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`objectId`, `product_name`, `product_quantity`, `product_price`, `product_type`) VALUES
+(1, 'ALLOPURINOL tablet 300mg, 100/box. Sold per tab.', 5000, 10, 'Tablets and Capsules'),
+(2, 'ALUMINUM HYDROXIDE MAGNESIUM HYDROXIDE tablet 200mg/100mg, 100/box.Sold per tab.', 3000, 10, 'Tablets and Capsules');
 
 -- --------------------------------------------------------
 
@@ -61,7 +84,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `last_name` varchar(40) NOT NULL,
   `email` varchar(40) NOT NULL,
   PRIMARY KEY (`objectId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 --
 -- Dumping data for table `users`
@@ -78,7 +101,8 @@ INSERT INTO `users` (`objectId`, `username`, `password`, `first_name`, `last_nam
 (8, '', 'da39a3ee5e6b4b0d3255bfef95601890afd80709', '', '', ''),
 (9, '', 'da39a3ee5e6b4b0d3255bfef95601890afd80709', '', '', ''),
 (10, 'avramirez', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'av', 'ramirez', 'avramirez@dynamicobjx.com'),
-(11, 'testing', 'dc724af18fbdd4e59189f5fe768a5f8311527050', 'test', 'tis', 'testing@yahoo.com');
+(11, 'testing', 'dc724af18fbdd4e59189f5fe768a5f8311527050', 'test', 'tis', 'testing@yahoo.com'),
+(12, 'sad', 'a6c78cad8916fbc6e07cbcfd3c601d446798becd', 'sad', 'dsa', 'sda@yah.com');
 
 -- --------------------------------------------------------
 
@@ -95,16 +119,15 @@ CREATE TABLE IF NOT EXISTS `users_reservation` (
   PRIMARY KEY (`objectId`),
   KEY `serviceId` (`serviceId`),
   KEY `userId` (`userId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `users_reservation`
 --
 
 INSERT INTO `users_reservation` (`objectId`, `serviceId`, `userId`, `reserveDate`, `reserveTime`) VALUES
-(1, 1, 10, '11/13/2013', '11:00 AM'),
-(2, 4, 10, '11/13/2013', '6:00 AM'),
-(3, 2, 10, '11/02/2013', '8:00 AM');
+(5, 1, 10, '11/06/2013', '8:00 AM'),
+(6, 2, 10, '11/21/2013', '9:00 AM');
 
 --
 -- Constraints for dumped tables
@@ -114,8 +137,8 @@ INSERT INTO `users_reservation` (`objectId`, `serviceId`, `userId`, `reserveDate
 -- Constraints for table `users_reservation`
 --
 ALTER TABLE `users_reservation`
-  ADD CONSTRAINT `users_reservation_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `users` (`objectId`),
-  ADD CONSTRAINT `users_reservation_ibfk_1` FOREIGN KEY (`serviceId`) REFERENCES `services` (`objectId`);
+  ADD CONSTRAINT `users_reservation_ibfk_1` FOREIGN KEY (`serviceId`) REFERENCES `services` (`objectId`),
+  ADD CONSTRAINT `users_reservation_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `users` (`objectId`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
