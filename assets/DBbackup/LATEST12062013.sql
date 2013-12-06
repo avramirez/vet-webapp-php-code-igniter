bd9025ff7f4478d0fc46461be8cd65d962e28d9b
Andrew James Ramirez <avramirez@dynamicobjx.com>
2013-12-06 18:54:56 +0800

    added new backup

--- assets/DBbackup/LATEST12062013.sql
+++ assets/DBbackup/LATEST12062013.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 06, 2013 at 01:01 AM
+-- Generation Time: Dec 06, 2013 at 11:53 AM
 -- Server version: 5.5.22
 -- PHP Version: 5.4.19
 
@@ -42,7 +42,7 @@ CREATE TABLE IF NOT EXISTS `products` (
 --
 
 INSERT INTO `products` (`objectId`, `product_name`, `product_quantity`, `product_price`, `product_type`) VALUES
-(1, 'ALLOPURINOL tablet 300mg, 100/box. Sold per tab.', 4424, 10, 'Tablets and Capsules'),
+(1, 'ALLOPURINOL tablet 300mg, 100/box. Sold per tab.', 5000, 10, 'Tablets and Capsules'),
 (2, 'ALUMINUM HYDROXIDE MAGNESIUM HYDROXIDE tablet 200mg/100mg, 100/box.Sold per tab.', 2997, 10, 'Tablets and Capsules');
 
 -- --------------------------------------------------------
@@ -86,7 +86,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `user_level` int(11) DEFAULT NULL,
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`objectId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
 
 --
 -- Dumping data for table `users`
@@ -96,7 +96,8 @@ INSERT INTO `users` (`objectId`, `username`, `password`, `first_name`, `last_nam
 (22, 'test', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'test', 'test', 'test@test.com', 1, '2013-12-05 17:11:09'),
 (23, 'superadmin', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'superadmin', 'superadmin', 'superadmin@admin.com', 2, '2013-12-05 17:14:09'),
 (24, 'adminuser', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'adminuser', 'adminuser', 'adminuser@admin.com', 3, '2013-12-05 17:16:10'),
-(25, 'adminreserve', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'adminreserve', 'adminreserve', 'adminreserve@yahoo.com', 4, '2013-12-05 19:17:16');
+(25, 'adminreserve', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'adminreserve', 'adminreserve', 'adminreserve@yahoo.com', 4, '2013-12-05 19:17:16'),
+(26, 'test2', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'test2', 'test2', 'test2@test.com', 1, '2013-12-06 01:17:29');
 
 -- --------------------------------------------------------
 
@@ -114,7 +115,33 @@ CREATE TABLE IF NOT EXISTS `users_order` (
   PRIMARY KEY (`objectId`),
   KEY `productId` (`productId`),
   KEY `usersId` (`usersId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=28 ;
+
+--
+-- Dumping data for table `users_order`
+--
+
+INSERT INTO `users_order` (`objectId`, `productId`, `usersId`, `productAmount`, `totalPrice`, `orderDate`) VALUES
+(8, 1, 26, 5001, 50010, '2013-12-06 02:29:38'),
+(9, 1, 26, 5001, 50010, '2013-12-06 02:29:40'),
+(10, 1, 26, 5001, 50010, '2013-12-06 02:34:49'),
+(11, 1, 26, 5001, 50010, '2013-12-06 02:35:52'),
+(12, 1, 26, 5001, 50010, '2013-12-06 02:35:53'),
+(13, 1, 26, 5001, 50010, '2013-12-06 02:35:53'),
+(14, 1, 26, 5001, 50010, '2013-12-06 02:35:54'),
+(15, 1, 26, 5001, 50010, '2013-12-06 02:35:54'),
+(16, 1, 26, 5001, 50010, '2013-12-06 02:35:54'),
+(17, 1, 26, 5001, 50010, '2013-12-06 02:35:54'),
+(18, 1, 26, 5001, 50010, '2013-12-06 02:36:21'),
+(19, 1, 26, 5001, 50010, '2013-12-06 02:36:22'),
+(20, 1, 26, 5001, 50010, '2013-12-06 02:36:22'),
+(21, 1, 26, 5001, 50010, '2013-12-06 02:36:22'),
+(22, 1, 26, 5001, 50010, '2013-12-06 02:36:22'),
+(23, 1, 26, 5001, 50010, '2013-12-06 02:36:23'),
+(24, 1, 26, 5001, 50010, '2013-12-06 02:36:23'),
+(25, 1, 26, 5001, 50010, '2013-12-06 02:36:23'),
+(26, 1, 26, 5001, 50010, '2013-12-06 02:36:23'),
+(27, 1, 26, 5001, 50010, '2013-12-06 02:37:20');
 
 -- --------------------------------------------------------
 
@@ -133,14 +160,15 @@ CREATE TABLE IF NOT EXISTS `users_reservation` (
   PRIMARY KEY (`objectId`),
   KEY `serviceId` (`serviceId`),
   KEY `userId` (`userId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=25 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
 
 --
 -- Dumping data for table `users_reservation`
 --
 
 INSERT INTO `users_reservation` (`objectId`, `serviceId`, `userId`, `reserveDate`, `reserveTime`, `reserveDateTime`, `confirmed`) VALUES
-(24, 1, 22, '12/18/2013', '2:00 PM', '2013-12-18 14:00:00', 0);
+(24, 1, 22, '12/18/2013', '2:00 PM', '2013-12-18 14:00:00', 0),
+(25, 1, 26, '12/19/2013', '12:00 PM', '2013-12-19 12:00:00', 0);
 
 --
 -- Constraints for dumped tables
