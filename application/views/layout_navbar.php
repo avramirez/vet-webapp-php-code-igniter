77e1b680ff716fd86154446e799827c9555c4557
fanboydesigns <jorelkimcruz@yahoo.com>
2014-01-05 17:06:31 +0800

    Added services, contact, about page
    Added services, contact, about page

--- application/views/layout_navbar.php
+++ application/views/layout_navbar.php
@@ -1,10 +1,19 @@
  <div class="masthead">
-        <h3 class="text-muted">Project name</h3>
+ <table>
+ <tr>
+ <td>
+  <img style="width:100px;" src="<?php echo base_url();?>assets/images/logo.jpg"></td>
+  <td>	   <h1 style="margin:10px 0px;">Vets In Practice</h1></td>
+  <td></td>
+ </tr>
+ </table>
+      
+
         <ul class="nav nav-justified navMainLayout">
           <li id="navHome"><a href="<?php echo base_url();?>">Home</a></li>
-          <li><a href="<?php echo base_url();?>">About</a></li>
-          <li><a href="<?php echo base_url();?>">Services</a></li>
-          <li><a href="<?php echo base_url();?>">Contact</a></li>
+          <li><a href="<?php echo base_url();?>about">About</a></li>
+          <li><a href="<?php echo base_url();?>services">Services</a></li>
+          <li><a href="<?php echo base_url();?>contact">Contact</a></li>
           <li id="navSignin"><a href="<?php echo base_url();?>signin">Sign-In</a></li>
           <li id="navUserRegister"><a href="<?php echo base_url();?>registration">Register</a></li>
         </ul>
